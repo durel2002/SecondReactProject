516b71011347eaa8865770f1a132c40b5ca760d6
durel2002 <durelhervane@gmail.com>
2023-08-29 10:08:47 +0100

    this app is working and it is the undo of the previous commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React,{useId}  from 'react';
-
 import './App.css';
 //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
-// we used useId to generate a unique id
-const useid = useId();
 function App() {
   return (
    <div className="form">
    <form>
-      <label htmlFor={useid}>Enter your name </label>
-     <input id={useid}  type="text" placeholder="UserName" /><br/>
+     <input  type="text" placeholder="UserName" /><br/>
      <input type="Password" placeholder="Password"/><br/>
      <p> Forgot Password? </p>
      <button type="submit"> Submit </button>
